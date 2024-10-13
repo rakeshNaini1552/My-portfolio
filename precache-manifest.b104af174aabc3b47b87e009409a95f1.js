f332968dc701dfaaea354c6a0ee06668a84678ad
Rakesh Naini <rakeshnaini155@gmail.com>
2024-10-13 13:51:33 -0500

    Updates

---
+++ precache-manifest.b104af174aabc3b47b87e009409a95f1.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1615f9e21e0898f55e87884db41605eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29cc40a60401db586d59",
+    "url": "/static/css/3.76dfe5d7.chunk.css"
+  },
+  {
+    "revision": "0fe4bf81657b0a720fbc",
+    "url": "/static/css/4.3a5b41b5.chunk.css"
+  },
+  {
+    "revision": "899110b11da8d80981bb",
+    "url": "/static/css/main.78d647c4.chunk.css"
+  },
+  {
+    "revision": "ae7302018ea1317d4355",
+    "url": "/static/js/2.6cfe3784.chunk.js"
+  },
+  {
+    "revision": "a6c0177ea3999624a9f2d92abf620885",
+    "url": "/static/js/2.6cfe3784.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29cc40a60401db586d59",
+    "url": "/static/js/3.22fd4b33.chunk.js"
+  },
+  {
+    "revision": "0fe4bf81657b0a720fbc",
+    "url": "/static/js/4.0320dc59.chunk.js"
+  },
+  {
+    "revision": "899110b11da8d80981bb",
+    "url": "/static/js/main.42197ea4.chunk.js"
+  },
+  {
+    "revision": "945eb6cea274b75aab25",
+    "url": "/static/js/runtime-main.5c3cad32.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "4bfa453f8148308e85504c03f5a72665",
+    "url": "/static/media/ctsLogo.4bfa453f.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "70fbbed203a4d5dc71732e865d21c442",
+    "url": "/static/media/rakesh.70fbbed2.jpg"
+  },
+  {
+    "revision": "d52de6098a5b8a0f73b025aebe3946d0",
+    "url": "/static/media/resume.d52de609.pdf"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "53fdf0cbc1cde870068d881a498ba5fd",
+    "url": "/static/media/umkc-logo.53fdf0cb.png"
+  }
+]);
